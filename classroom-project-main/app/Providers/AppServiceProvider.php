1ef0f4b1b37d5b7f043aa3b98450c224dea04625
Abdelrah3110-man <abdo01064348224@gmail.com>
2026-05-13 05:36:18 +0300

    update

--- classroom-project-main/app/Providers/AppServiceProvider.php
+++ classroom-project-main/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Classroom;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         View::composer('*', function ($view) {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        View::composer('*', function ($view) {
 
         if (auth()->check()) {
 
